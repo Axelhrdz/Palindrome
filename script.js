1f9e580ceea3bd80d29ab96cf27e82211022b187
Axelhrdz <axelhernandezperez23@gmail.com>
2024-11-26 21:44:32 -0600

    working on app logic

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 const input = document.getElementById('text-input');
 const submit = document.getElementById('check-btn');
-const form = document.getElementById('form');
-console.log(form)
+// const form = document.getElementById('form');
+const result = document.getElementById('result');
+// console.log(form)
+console.log(result);
 
 
 //Takin word from the input text
 
 const palindrome = (word) => {
     console.log(word);
-    console.log(word.split(''));
-    console.log(word.length);
+    // console.log(word.split(''));
+    // console.log(word.length);
+    const wordChars = word.split('');
+    const wordReversed = word.split('').reverse();
 
-    if(word.length === 1){
+    console.log(wordChars);
+    console.log(wordReversed);
+    console.log(wordReversed[wordReversed.length -2]);
+    
+    if(!input.value){
+        alert('Please input a value');
+    } else if (word.length === 1){
         console.log(true);
+        result.innerText = `${word} is a palindrome `        
     }
 }
 
-form.addEventListener('submit', function(e){
-    e.preventDefault();
+submit.addEventListener('click', function(e){
+    // e.preventDefault();
     palindrome(input.value);
 })
 
