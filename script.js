9873dc88f2398dbba21a452c761e4280e6c9e1ce
Axelhrdz <axelhernandezperez23@gmail.com>
2024-12-02 22:53:40 -0600

    working on function validations

--- script.js
+++ script.js
@@ -7,25 +7,33 @@ console.log(result);
 
 
 //Comparing input text with reversed one
+let stat;
 const compare = (regular, reversed) => {
     console.log(regular);
     console.log(reversed);
     
+    
     regular.forEach((ele, index) => {
       if(ele === reversed[index]){
         console.log(true)
+        stat = true;
       } else {
+        stat = false;
         console.log(false)
       }
     })
+    if(stat){
+        result.innerText = `${regular.join('')} IS a palindrome`
+    } else {
+        result.innerText = `${regular.join('')} is NOT a palindrome`
+    }
 }
 
 
 
 //Takin word from the input text
 const palindrome = (word) => {
-    let status = false;
-
+    
     console.log(word);
     // console.log(word.split(''));
     // console.log(word.length);
