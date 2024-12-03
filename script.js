29e082bd62f889da59e7a6406382c31829165201
Axelhrdz <axelhernandezperez23@gmail.com>
2024-12-02 22:45:17 -0600

    adding compare function

--- script.js
+++ script.js
@@ -6,30 +6,50 @@ const result = document.getElementById('result');
 console.log(result);
 
 
-//Takin word from the input text
+//Comparing input text with reversed one
+const compare = (regular, reversed) => {
+    console.log(regular);
+    console.log(reversed);
+    
+    regular.forEach((ele, index) => {
+      if(ele === reversed[index]){
+        console.log(true)
+      } else {
+        console.log(false)
+      }
+    })
+}
 
+
+
+//Takin word from the input text
 const palindrome = (word) => {
+    let status = false;
+
     console.log(word);
     // console.log(word.split(''));
     // console.log(word.length);
     const wordChars = word.split('');
     const wordReversed = word.split('').reverse();
 
-    console.log(wordChars);
-    console.log(wordReversed);
-    console.log(wordReversed[wordReversed.length -2]);
+    // console.log(wordChars);
+    // console.log(wordReversed);
+    // console.log(wordReversed[wordReversed.length -1]);
     
     if(!input.value){
         alert('Please input a value');
     } else if (word.length === 1){
         console.log(true);
         result.innerText = `${word} is a palindrome `        
+    } else {
+        compare(wordChars, wordReversed);
     }
 }
 
 submit.addEventListener('click', function(e){
     // e.preventDefault();
     palindrome(input.value);
+    
 })
 
-//splitting word
+
